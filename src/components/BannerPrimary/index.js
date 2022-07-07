f9e31082410ec4565cdc8dc70c3525b67b6da765
Roberto Juan Santos da Silva <ROBERTOJUANSANTOSDASILVA@GMAIL.COM>
2022-07-07 20:31:52 -0300

    test

--- src/components/BannerPrimary/index.js
+++ src/components/BannerPrimary/index.js
@@ -2,26 +2,27 @@ import React from "react"
 import * as S from "./styles"
 import SimpleImageSlider from "react-simple-image-slider"
 import banner_01 from "../../assets/banner_01.jpg"
-//  import banner_02 from "../../assets/banner_02.jpg"
+import banner_02 from "../../assets/banner_02.jpg"
 
 const images = [
-    { url: {banner_01} },
-    { url: "../../assets/banner_02.jpg" },
-  ];
-  
+    require({banner_01}),
+    require({banner_02}),
+];
 
 export default function BannerPrimary(){
     return(
         
         <S.Container>
             <S.Center>
-                <SimpleImageSlider
-                    width={1900}
-                    height={504}
-                    images={images}
-                    showBullets={true}
-                    showNavs={true}
-                />
+                
+            <SimpleImageSlider
+            width='100%'
+            height={500}
+            images={images}
+            showBullets={true}
+            showNavs={true}
+            />
+                
             </S.Center>
         </S.Container>
     )
--- src/components/BannerPrimary/styles.js
+++ src/components/BannerPrimary/styles.js
@@ -3,9 +3,14 @@ import styled from "styled-components"
 export const Container = styled.div`
 display: flex;
 border-bottom: 5px solid #cac775;
+
 `
 
 export const Center = styled.div`
     display: flex;
     
+    images {
+        width: 100%;
+        height: 500px;
+    }
 `
