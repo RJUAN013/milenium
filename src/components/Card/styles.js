2ffa481a8c8aaf6b74a7a44a671a2a00a6243f16
Roberto Juan Santos da Silva <ROBERTOJUANSANTOSDASILVA@GMAIL.COM>
2022-07-08 18:24:28 -0300

    teste

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components"
 export const Container = styled.div`
     width: 100%;
     height: 200px;
-    padding: 10px;
     display: flex;
     justify-content: center;
 `
